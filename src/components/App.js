e10f9183b0b410d31ec286c4245a45254db4bacf
DhanushM12 <dhanushmukhi@gmail.com>
2020-09-26 12:18:28 +0530

    created folder structure and added material ui library

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="top">Machine Test - Nimap</h1>
+    </div>
+  );
+}
+
+export default App;
